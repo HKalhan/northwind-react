018570e20e1990583e685422e3cd8417f8823ebf
HKalhan <hasret.kalhann@gmail.com>
2021-06-28 17:24:12 +0300

    update

--- src/layouts/Navi.js
+++ src/layouts/Navi.js
@@ -1,45 +1,38 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router'
 import { Container, Menu } from 'semantic-ui-react'
-
-import CartSummary from './CartSummary'
+import {useSelector} from "react-redux"
+ import CartSummary from './CartSummary'
 import SignedIn from './SignedIn'
 import SignedOut from './SignedOut'
 
 export default function Navi() {
+    const {cartItems} = useSelector(state => state.cart)
     const [isAuthenticated, setIsAuthenticated] = useState(true)
-    const history=useHistory()
-
-    function handleSignOut(){
-        setIsAuthenticated(false)
-        history.push("/")
+    const history = useHistory()
+    function handleSignOut() {
+      setIsAuthenticated(false)
+      history.push("/")
     }
-
-    function handleSignIn(){
-        setIsAuthenticated(true)
+  
+    function handleSignIn() {
+      setIsAuthenticated(true)
     }
-
+  
     return (
-        <div>
-            <Menu inverted fixed="top">
-                <Container>
-                    <Menu.Item
-                        name='home'
-
-                    />
-                    <Menu.Item
-                        name='messages'
-
-                    />
-
-                    <Menu.Menu position='right'>
-                        <CartSummary />
-                        {isAuthenticated?<SignedIn signOut={handleSignOut}/>:<SignedOut signIn={handleSignIn}/>}
-                      
-                    </Menu.Menu>
-                </Container>
-
-            </Menu>
-        </div>
-    )
-}
+      <div>
+        <Menu inverted fixed="top">
+          <Container>
+            <Menu.Item name="home" />
+            <Menu.Item name="messages" />
+  
+            <Menu.Menu position="right">
+              {cartItems.length>0&&<CartSummary/>}
+              {isAuthenticated?<SignedIn signOut={handleSignOut} bisey="1"/>
+              :<SignedOut signIn={handleSignIn}/>}  
+            </Menu.Menu>
+          </Container>
+        </Menu>
+      </div>
+    );
+  }
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,9 +1,14 @@
 import React, {useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import { Icon, Label, Menu, Table, Button } from 'semantic-ui-react'
 import ProductService from '../productService'
+import { useDispatch } from 'react-redux'
+import {addToCart} from "../store/actions/cartActions"
+import {toast} from "react-toastify"
 
 export default function ProductList() {
+
+    const dispatch = useDispatch()
 
     const [products, setProducts] = useState([])
 
@@ -12,6 +17,12 @@ export default function ProductList() {
        productService.getProducts().then(result=>setProducts(result.data.data))
         
     }, [])
+
+
+    const handleAddToCart=(product)=>{
+        dispatch(addToCart(product));
+        toast.success(`${product.productName} sepete eklendi!`)
+    };
 
     return (
         <div>
@@ -23,6 +34,7 @@ export default function ProductList() {
                         <Table.HeaderCell>Stok Adedi</Table.HeaderCell>
                         <Table.HeaderCell>Açıklama</Table.HeaderCell>
                         <Table.HeaderCell>Kategori</Table.HeaderCell>
+                        <Table.HeaderCell></Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
@@ -30,11 +42,15 @@ export default function ProductList() {
                     {
                         products.map(product=>(
                             <Table.Row key={product.id}>
-                            <Table.Cell><Link to={`/products/${product.name}`}>{product.productName}</Link></Table.Cell>
+                            <Table.Cell>
+                                <Link to={`/products/${product.productName}`}>
+                                    {product.productName}
+                                    </Link></Table.Cell>
                             <Table.Cell>{product.unitPrice}</Table.Cell>
                             <Table.Cell>{product.unitsInStock}</Table.Cell>
                             <Table.Cell>{product.quantityPerUnit}</Table.Cell>
                             <Table.Cell>{product.category.categoryName}</Table.Cell>
+                            <Table.Cell><Button onClick={()=>handleAddToCart(product)}> Sepete Ekle</Button></Table.Cell>
                         </Table.Row>    
 
                         ))
